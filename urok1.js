e70562712a7390426b1c64cb9322c14bb833c9c8
Demonss11 <demonss11@gmail.com>
2018-02-25 23:17:44 +0300

    на25.02

--- urok1.js
+++ urok1.js
@@ -1,1 +1,15 @@
-parseInt('1001011',2);
+function twoToTen(sTwo){
+    let l = sTwo.lenght - 1;
+    nInt = 0;
+    for(i=0;i<=l;i++){
+        if(sTwo[i] == '1'){
+            nInt = nInt + Math.pow(2,l-i);
+        } else if(sTwo[i] == '0'){
+            //'nothing'
+        }else{
+            return;
+        }
+    }
+    return nInt;
+}
+console.log(twoToTen('1001011'));
